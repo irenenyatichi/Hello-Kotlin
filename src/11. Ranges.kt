2f1146b0317d2deecb06ebbbc7b0c73bb3140bd2
Irene Nyatichi <79199068+irenenyatichi@users.noreply.github.com>
2024-01-24 15:49:22 +0300

    11. Ranges.kt

--- src/11. Ranges.kt
+++ src/11. Ranges.kt
@@ -3,5 +3,5 @@
 // To declare a range in reverse order, use downTo. For example, 4 downTo 1 is equivalent to 4, 3, 2, 1.
 // To declare a range that increments in a step that isn't 1, use step and your desired increment value. For example, 1..5 step 2 is equivalent to 1, 3, 5.
 // You can also do the same with Char ranges:
-// 'a'..'d' is equivalent to 'a', 'b', 'c', 'd'
-// 'z' downTo 's' step 2 is equivalent to 'z', 'x', 'v', 't'
+//   'a'..'d' is equivalent to 'a', 'b', 'c', 'd'
+//   'z' downTo 's' step 2 is equivalent to 'z', 'x', 'v', 't'
